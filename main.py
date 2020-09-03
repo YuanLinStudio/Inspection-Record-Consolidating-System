70979db868f792bc89532819518103d0cda37bff
Lin Yuan <yuanlin0921@gmail.com>
2020-09-02 20:27:00 -0700

    Preprocess

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import pandas
 import numpy
 
 from readIn import readIn
-from preProcess import preProcess
+from Preprocess import Preprocess
 
 originalDataFrame = readIn('4165188.csv')
 
-originalDataFrame = preProcess(originalDataFrame)
+preprocess = Preprocess(originalDataFrame)
 
+preprocess.preprocess()
+
+originalDataFrame = preprocess.dataFrame
+
+
+print(originalDataFrame.info())
 '''
 print(originalDataFrame['开始答题时间'][4]>originalDataFrame['开始答题时间'][5])
-print(originalDataFrame.info())
 print(originalDataFrame['开始答题时间'])
 print(originalDataFrame)
 print(originalDataFrame['检查日期'])
 '''
-originalDataFrame.to_csv('1.csv', encoding="utf_8_sig")
-print(originalDataFrame.columns.values)
+# originalDataFrame.to_csv('1.csv', encoding="utf_8_sig")
+# print(originalDataFrame.columns.values)
--- preProcess.py
+++ preProcess.py
@@ -2,22 +2,73 @@ import pandas
 from datetime import datetime
 
 
-def preProcess(dataFrame):
+class Preprocess:
 
-    # 列识别
+    dataFrame = []  # 列
 
-    # 检查日期多列合并
-    dataFrame['date'] = dataFrame['2.检查日期:请选择'] + \
-        dataFrame['2.检查日期:请选择.1'] + dataFrame['2.检查日期:请选择.2']
+    def __init__(self, dataFrame):
 
-    # 检查日期“今天”的替换
-    dataFrame['检查日期'] = dataFrame['开始答题时间'].dt.date
-    for index, row in dataFrame.iterrows():
-        if row['date'] != '今天':
-            dataFrame.loc[index, '检查日期'] = datetime.strptime(
-                row['date'], '%Y年%m月%d日').date()
+        self.dataFrame = dataFrame
 
-    # 删除备用列
-    dataFrame.drop(['date'], axis=1, inplace=True)
+    def preprocess(self):
 
-    return dataFrame
+        # 列合并
+        self.columnMerge()
+
+        # 重命名列
+        self.columnRename()
+
+        '''
+        # 检查日期“今天”的替换
+        dataFrame['检查日期'] = dataFrame['开始答题时间'].dt.date
+        for index, row in dataFrame.iterrows():
+            if row['date'] != '今天':
+                dataFrame.loc[index, '检查日期'] = datetime.strptime(
+                    row['date'], '%Y年%m月%d日').date()
+
+        # 删除备用列
+        dataFrame.drop(['date'], axis=1, inplace=True)'''
+
+    '''
+    def columnRecognize(dataFrame) -> dict:
+
+        columnIndex = dict()
+
+        columnNames = dataFrame.columns.tolist()
+
+        columnNamesToRecognize = ['开始答题时间']
+
+        columnIndex['开始答题时间'] = [i for i, j in enumerate(columnNames) if '开始答题时间' in j]
+        columnIndex['检查日期'] = [i for i, j in enumerate(columnNames) if '检查日期' in j]
+
+        return columnIndex
+    '''
+
+    def columnRename(self):
+
+        columnRenameDict = {'编号': '原始记录编号',
+                            '1.服务监督员编号或姓名': '服务监督员编号或姓名',
+                            '3.检查地点:请选择': '检查地点位置',
+                            '3.检查地点:请选择.1': '检查地点线路',
+                            '3.检查地点:请选择.2': '检查地点站点',
+                            '4.您本次检查是否发现了问题？': '是否发现问题',
+                            '6.问题描述': '问题描述',
+                            '7.问题类型及分类:请选择': '问题类型',
+                            '7.问题类型及分类:请选择.1': '问题分类',
+                            '8.备注': '监督员备注',
+                            '9.请您选择本次检查共计划提交几份附件': '附件个数',
+                            '10.附件': '附件 1',
+                            '11.附件': '附件 2'}
+
+        self.dataFrame.rename(columns=columnRenameDict, inplace=True)
+
+    def columnMerge(self):
+
+        # 检查日期多列合并
+        self.dataFrame['日期'] = self.dataFrame['2.检查日期:请选择'] + \
+            self.dataFrame['2.检查日期:请选择.1'] + self.dataFrame['2.检查日期:请选择.2']
+
+        # 发生时间多列合并
+        self.dataFrame['时间'] = self.dataFrame['5.发生时间（或发现时间）:请选择'] + \
+            self.dataFrame['5.发生时间（或发现时间）:请选择.1']
+
